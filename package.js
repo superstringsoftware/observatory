b0d97b1eceb89c03358d2708102acdc2ab6c452a
J Ho Xray <jho.xray@gmail.com>
2013-03-18 21:27:05 +0100

    moving testing to a separate package

--- package.js
+++ package.js
@@ -8,7 +8,6 @@ Package.on_use(function (api) {
 
   api.add_files('logging/observatory.coffee','server');
   api.add_files('logging/lib/inspect.js','server');
-  api.add_files('testing/client/lib/chai.js','client');
   api.add_files('client/codemirror/codemirror.js','client');
   api.add_files('client/codemirror/javascript.js','client');
   api.add_files('client/codemirror/codemirror.css','client');
@@ -40,14 +39,9 @@ Package.on_use(function (api) {
     'logging/client/observatory.html',
     'logging/client/observatoryTemplates.coffee',
     'logging/client/observatory.less',
-    'testing/mtest.coffee',
-    'testing/sampleTests.coffee',
-    'testing/client/simpleTests.html',
-    'testing/client/mtestsTemplates.coffee'
+
   ], 'client');
 
-  api.add_files('testing/mtest.coffee','server');
-  api.add_files('testing/sampleTests.coffee','server');
 });
 }
 catch (err) {
