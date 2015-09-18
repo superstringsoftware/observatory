21b6d48716e7acce6efab28ae06681caad5b8231
Vitaly Nikitin <Vitaly.Nikitin@veeam.com>
2015-09-18 11:31:00 +0200

    fix package version for local install

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 try {
     Package.describe({
         "summary": "Versatile and powerful logging and application management (http://observatoryjs.com)",
-        "version": "0.4.9-rc.1_1",
+        "version": "0.4.9",
         "name": 'superstringsoft:observatory-beta',
         "git": "https://github.com/superstringsoftware/observatory.git"
     });
